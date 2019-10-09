b78bd56d404875b7e275e78a0da485c95c281e7f
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-09 22:02:40 +0200

    React Components still  work on

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -3,15 +3,15 @@ import Dzielo from './Dzielo';
 import Formul from './Formul';
 import Result from './Result';
 import Weather from './Weather';
-
+import './App.css';
 import Tytu from './Tytu';
 // import 'axios' from axios;
-import './App.css';
 //import PropTypes from 'prop-types'
 
 const APIKey ='05508bb378ad891b493b0c886cca7a57';
 
-class App extends Component {     
+class App extends React.Component {  
+
   state = {
       value:'',
       city: '',
@@ -26,66 +26,34 @@ class App extends Component {
       sunset:'',
       error:''
     }
+
+
+
+    getWeather = async () => {
+    const API_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=&{APIKey}$units=metric');
     
-    
-  handleInputChange =(e) => {
-    this.setState({
-      value: e.target.value
-    })
+    const data = await API_call.json();  //JSON.stringify (value: any, space: any);
+
+    console.log(data);
+
   }
 
-  handleCitySubmit = e => {
-    e.preventDefault()
-    console.log('Potwiedzono');
-    const API = 'http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=&{APIKey}$units=metric';
-
-    fetch (API)  // obietnica ,która bedzie czekać na roztrzygnicie 
-      .then( response => { 
-        if (response.ok) { 
-          return response
-        }
-        else {
-          throw Error('No Error no')
-        }
-    })
-      .then(response => response.json() )
-
-      .then (data => {
-        console.log(data);
-      } )
-    /*  .then( () =>this.setState({ error: false}) )
-      .catch(error => { console.log(error); this.setState({ error:false })  
-      .then(data =>{
-        const date = new Date().toLocaleString()
-        this.setState({
-          
-          city: '',
-          geolo:'',
-          data:'',
-          wet:'',
-          temp:'',
-          rain:'',
-          press:'',
-          wind:'',
-          error:'',
-        
-      })  */
-  }  
 
 
-render() {
-  return ( 
-  <div className ="App"> 
-        <Dzielo 
-          value = {this.state.value }
-          change={this.handleInputChange} 
-          submit={this.handleCitySubmit} 
-        />
-        <Result error={this.state.error} />
-          
-  </div>
-        );
-      }
+    render () {
+      return (
+        <div>
+          <Formul />
+          <Weather />
+          <Dzielo />
+          <Formul getWeather={this.getWeather} />
+        </div>
+      )
     }
+      
 
-      export default App;
+  
+  }
+
+export default App;
+
--- weather-app/src/components/Formul.js
+++ weather-app/src/components/Formul.js
@@ -3,9 +3,22 @@ import React from "react";
 class Formul extends React.Component {
 
   render() {
-    
+
     return (
-      <div> Component pogodowy</div>
+      <form> onSubmit={this.props.getWeather} >
+        <input type="text" name="city" placeholder="City" />
+        <input type="text" name="Country" placeholder="Country" />
+        <input type="text" name="geolo" placeholder="geolo" />
+        <input type="text" name="wet" placeholder="wet" />
+        <input type="text" name="temp" placeholder="temp" />
+        <input type="text" name="press" placeholder="press" />
+        <input type="text" name="rain" placeholder="rain" />
+        <input type="text" name="wind" placeholder="wind" />
+        <input type="text" name="sunrise" placeholder="sunrise" />
+
+        <button>Jaka jest pogoda ?</button>
+
+      </form>
     );
   }
 };
