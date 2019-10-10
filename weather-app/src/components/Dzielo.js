8109706abe035dda0043400c900db91ad2460b29
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-11 01:18:40 +0200

    Er

--- weather-app/src/components/Dzielo.js
+++ weather-app/src/components/Dzielo.js
@@ -3,10 +3,13 @@ import React from 'react';
 //twór komp funkcyjny zastąpię   input type=              <input type="text" value={ props.value }  
      // onChange={props.value} 
      // placeholder = ""  />     8 line
-const Dzielo = props =>   {              
-  return (                           
+const Dzielo = props =>   {     
+  
+  //const { data, city, sunrise,sunset,temp, press, temp,wind,error } = props.weather;
+  return (    
+    
   <form onSubmit={props.submit} >
-     
+
     <div>
       <h3>Wyszukiwarka Pogody</h3> 
       <p>Dowiedz się jaka jest temperatura i wiele więcej</p>
