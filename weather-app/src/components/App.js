3f5a3a38314a45ac0ca72155c926de27ec74637a
Agnieszka <54471623+Auleo@users.noreply.github.com>
2019-10-22 16:31:21 +0200

    Update App.js

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Form from './Form';
+import './App.css';
+import SearchCity from './SearchCity';
 import Head from './Head';
 import Wynik from './Wynik';
-import './App.css';
+
 
 const api_key = "05508bb378ad891b493b0c886cca7a57";
 class App extends React.Component {
@@ -15,9 +16,9 @@ class App extends React.Component {
     wind:"",
     humidity:"",
     geolo:"",
-    description:"",
-    wet:"",
+    latitude:"",
     sunrise:"",
+    wet:"",
     sunset:"",
     press:"",
     rain:"",
@@ -29,17 +30,26 @@ class App extends React.Component {
     const country =e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}$units=imperial$appid=${api_key}`);
-                                  
+  /*
+    iniMaop =() => {
+    const map =new google.maps.MAp(document.getElementById('map'), {
+        center: {lat: -34.397, lang: 150.644},
+        zoom: 8
+      });
+    }    
+*/
+
     const response = await api_call.json();
     console.log(response);
     if(city && country){
-      this.setState({
+      this.setState({        
         temperature: response.main.temperature,
         city:response.name,
         humidity:response.main.humidity,
         pressure:response.main.pressure,
         wind:response.main.wind,
         geolo:response.main.geolo,
+
         description: response.weather[0].description,
         wet:response.main.wet,
         sunrise:response.main.sunrise,
@@ -55,15 +65,15 @@ class App extends React.Component {
 }
 }
 
-
 render() {
   return (
     <div className="container">
+      <div id="map"></div>
       <div className="header">
         <Head/>
         </div>
         <div className="form">
-          <Form loadWeather = {this.getWeather}  />
+          <SearchCity loadWeather = {this.getWeather}  />
           </div>
             <div className="weather">
               <Wynik
